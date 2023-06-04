67cbb9847ac41400b872b1d08afe1498a14e740d
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2023-06-04 14:53:12 +0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,9 @@
 let a = prompt ("What is your age?");
 
 a = Number.parseInt(a);
-// let b = alert ("Hay");
+
 if(a>0){
     alert("This is a valid age :) ")
-
 }
 else{
     alert ("This is not  a valid age :(")
